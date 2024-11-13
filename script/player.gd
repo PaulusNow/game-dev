1121bc0800c6f930ffea1cff10b2f3ae246bf2b7
Paulus William <98863022+PaulusNow@users.noreply.github.com>
2024-11-13 15:58:53 +0700

    menambahkan dialogue ke npc

--- script/player.gd
+++ script/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 # Variables
 @export var speed = 100
 
+var oldman_in_range = false
+
 # Input for movement
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -13,6 +15,11 @@ func get_input():
 
 # Movement & Animation
 func _physics_process(delta):
+	
+	if oldman_in_range == true:
+		if Input.is_action_just_pressed("oke"):
+			DialogueManager.show_example_dialogue_balloon(load("res://dialog/dialogue.dialogue"), "start")
+			return
 	get_input()
 	move_and_slide()  # Tambahkan velocity sebagai parameter
 	update_animation()
@@ -28,3 +35,12 @@ func update_animation():
 		# Menentukan arah flip berdasarkan velocity
 		animated_sprite.flip_h = velocity.x < 0  # Balik secara horizontal jika bergerak ke kiri
 		velocity.y > 0 and abs(velocity.y) > abs(velocity.x)  # Balik secara vertikal jika bergerak ke bawah
+
+
+func _on_detection_body_entered(body: Node2D) -> void:
+	if body.has_method("old_man"):
+		oldman_in_range = true
+		
+func _on_detection_body_exited(body: Node2D) -> void:
+	if body.has_method("old_man"):
+		oldman_in_range = false
